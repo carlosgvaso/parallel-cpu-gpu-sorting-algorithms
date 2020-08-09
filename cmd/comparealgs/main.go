6608297b99890fd49e6cfe88fb13f6d22f103cd5
Utkarsh Vardan <uv375@login4.stampede2.tacc.utexas.edu>
2020-08-09 11:31:45 -0500

    Adding readfile method

--- cmd/comparealgs/main.go
+++ cmd/comparealgs/main.go
@@ -6,10 +6,12 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"runtime"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 
@@ -97,6 +99,33 @@ func readInput(inFile string) ([]int, []int, int) {
 	waitGroup.Wait()
 
 	return arrIn, arrOut, n
+}
+
+func readFile(fname string) (nums []int, err error) {
+	b, err := ioutil.ReadFile(fname)
+	if err != nil {
+		return nil, err
+	}
+
+	lines := strings.Split(string(b), "\n")
+	// Assign cap to avoid resize on every append.
+	nums = make([]int, 0, len(lines))
+
+	for _, l := range lines {
+		// Empty line occurs at the end of the file when we use Split.
+		if len(l) == 0 {
+			continue
+		}
+		// Atoi better suits the job when we know exactly what we're dealing
+		// with. Scanf is the more general option.
+		n, err := strconv.Atoi(l)
+		if err != nil {
+			return nil, err
+		}
+		nums = append(nums, n)
+	}
+
+	return nums, nil
 }
 
 // Main reads the array in the input file, and records the execution times each
